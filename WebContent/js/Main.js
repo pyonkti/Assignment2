12abfa0efcabc61329bfed22c3844ce75f70fd18
pyonkti <751069606@qq.com>
2019-11-30 14:43:45 +0100

    画布布局调整，更多观众，延长跑道，加入靶场

--- WebContent/js/Main.js
+++ WebContent/js/Main.js
@@ -1,10 +1,11 @@
-var game = new Phaser.Game(1280, 578, Phaser.CANVAS, 'Javeline', { preload: preload, create: create, update: update, render: render });
-var whiteAd;
+var game = new Phaser.Game(1280, 575, Phaser.CANVAS, 'Javeline', { preload: preload, create: create, update: update, render: render });
 var audience = new Array(new Array(7),new Array(6),new Array(5));
 var background;
-var runway;
-var audienceSeat;
+var runway = new Array(2);
+var audienceSeat = new Array(3);
 var player;
+var cursors;
+var ground;
 
 function preload() {
     game.load.spritesheet('white_audience', 'assets/images/audience_white_sprite.png', 24,32);
@@ -15,56 +16,84 @@ function preload() {
     game.load.image('sky', 'assets/images/sky.png');
     game.load.image('audience_seat', 'assets/images/audience_seat.png');
     game.load.image('runway', 'assets/images/runway.png');
+    game.load.image('ground', 'assets/images/ground.png');
 }
+
 function create() {
 	game.stage.backgroundColor = "#3ed8fb";
 	game.physics.startSystem(Phaser.Physics.ARCADE);
-	game.world.camera.position.set(0);
-	background = game.add.tileSprite(0, 0, 1680, 840, 'sky');
+	game.physics.arcade.gravity.y = 654;
+	game.world.setBounds(0, 0, 8000, 3600);
+	background = game.add.tileSprite(0, 3000, 1680, 840, 'sky');
 	background.scale.set(0.8,0.7);
-	background.fixedToCamera = true;
-	runway = game.add.tileSprite(0, 157, 1650, 840, 'runway');
-	runway.scale.set(1,0.5);
-	audienceSeat = game.add.tileSprite(0, 12, 1650, 840, 'audience_seat');
-	audienceSeat.scale.set(0.8,0.8);
-	setAudience();
-	backAndForth();
-	player = game.add.sprite(50, 330,'player',0); 
-	player.animations.add('play');
-	player.animations.play('play', 10, true);
-	player.scale.set(6);
 	background.autoScroll(10,0);
+	runway[0] = game.add.tileSprite(0, 3180, 1650, 840, 'runway');
+	runway[0].scale.set(1,0.5);
+	runway[1] = game.add.tileSprite(1130, 3180, 1650, 840, 'runway');
+	runway[1].scale.set(1,0.5);
+	ground = game.add.tileSprite(2780, 3180, 1650, 840, 'ground');
+	ground.scale.set(1,0.5);
+	setAudience();
+	//backAndForth();
+	setPlayer();
+	cursors = game.input.keyboard.createCursorKeys();
+	game.camera.follow(player);
 }
 
 function setAudience(){
-	for(var i=0;i<3;i++)
-    {
-    	for(var j=0;j<audience[i].length;j++)
-    	{
-    		var randomAd = Math.floor(Math.random()*8)%4;
-    		var randomFr = Math.floor(Math.random()*3);
-    		if (randomAd == 0){
-    			audience[i][j] = game.add.sprite(418 + 64 * j+32*i, 80 + 80 * i,'white_audience');   			
-    		}
-    		else if (randomAd == 1){
-    			audience[i][j] = game.add.sprite(418 + 64 * j+32*i, 80 + 80 * i,'red_audience');
-    		}
-    		else if (randomAd == 2){
-    			audience[i][j] = game.add.sprite(418 + 64 * j+32*i, 80 + 80 * i,'yellow_audience');
-    		}
-    		else if (randomAd == 3){
-    			audience[i][j] = game.add.sprite(418 + 64 * j+32*i, 80 + 80 * i,'purple_audience');
-    		}
-    		audience[i][j].scale.set(4);
-    		audience[i][j].animations.add('cheer');
-    		audience[i][j].animations.play('cheer', 3, true);
-    		audience[i][j].animations.next(randomFr);
-    	}
-    }
+	for(var i=0 ; i<3; i++){
+		audienceSeat[i] = game.add.tileSprite(850*i, 3035, 1650, 840, 'audience_seat');
+		audienceSeat[i].scale.set(0.8,0.8);
+	}
+	for(var k=0; k<3;k++)
+	{
+		for(var i=0;i<3;i++)
+	    {
+	    	for(var j=0;j<audience[i].length;j++)
+	    	{
+	    		var randomAd = Math.floor(Math.random()*8)%4;
+	    		var randomFr = Math.floor(Math.random()*3);
+	    		if (randomAd == 0){
+	    			audience[i][j] = game.add.sprite(audienceSeat[k].x+420 + 64 * j+32*i, audienceSeat[k].y  + 80 * (i+1),'white_audience');   			
+	    		}
+	    		else if (randomAd == 1){
+	    			audience[i][j] = game.add.sprite(audienceSeat[k].x+420 + 64 * j+32*i, audienceSeat[k].y + 80 * (i+1),'red_audience');
+	    		}
+	    		else if (randomAd == 2){
+	    			audience[i][j] = game.add.sprite(audienceSeat[k].x+420 + 64 * j+32*i, audienceSeat[k].y +  80 * (i+1),'yellow_audience');
+	    		}
+	    		else if (randomAd == 3){
+	    			audience[i][j] = game.add.sprite(audienceSeat[k].x+420 + 64 * j+32*i, audienceSeat[k].y + 80 * (i+1),'purple_audience');
+	    		}
+	    		audience[i][j].scale.set(4);
+	    		audience[i][j].animations.add('cheer');
+	    		audience[i][j].animations.play('cheer', 3, true);
+	    		audience[i][j].animations.next(randomFr);
+	    	}
+	    }
+	}
+}
+
+function setPlayer(){
+	player = game.add.sprite(50, 3350,'player'); 
+	game.physics.enable(player,Phaser.Physics.ARCADE);
+	player.scale.set(6);
+	player.body.allowGravity = false;
+	player.animations.add('play');
+	player.animations.play('play', 10, true);
+	player.body.collideWorldBounds = true;
 }
 
 function update() {
-
+	player.body.velocity.set(0);
+	if (cursors.right.isDown)
+    {
+        player.body.velocity = new Phaser.Point(800,0);
+    }
+	else if (cursors.left.isDown)
+    {
+		player.body.velocity.set(-800,0);
+    }
 }
 
 function backAndForth(){
@@ -98,5 +127,12 @@ function backAndForth(){
 
 
 function render() {
-
+	var p_zone = player.getBounds();
+    	game.context.fillStyle = 'rgba(0,0,0,0.6)';
+    	game.context.fillRect(p_zone.x, p_zone.y, p_zone.width, p_zone.height);
+	 var zone = new Phaser.Rectangle(player.body.x,player.body.y,player.body.width,player.body.height);
+	    game.context.fillStyle = 'rgba(255,0,0,0.6)';
+	    game.context.fillRect(zone.x, zone.y, zone.width, zone.height);
+	   // game.debug.spriteBounds(player, 'pink', false);
+	game.debug.cameraInfo(game.camera, 32, 32);
 }
